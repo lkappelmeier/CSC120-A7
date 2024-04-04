5fe2976c4766991df2012c14844168b1344fedfb
lkappelmeier <lkappelmeier@smith.edu>
2024-04-03 22:29:15 -0400

    A7 Commit

--- CampusMap.java
+++ CampusMap.java
@@ -44,6 +44,16 @@ public class CampusMap {
         CampusMap myMap = new CampusMap();
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4));
+        myMap.addBuilding(new Building("Lawrence House", "99 Green Street", 4));
+        myMap.addBuilding(new Building("Albright House", "7 Bedford Terrace", 4));
+        myMap.addBuilding(new Building("Sage Hall", "144 Green Street", 4));
+        myMap.addBuilding(new Building("Campus Center", "100 Elm Street", 3));
+        myMap.addBuilding(new Building("Josten Library", "122 Green Street"));
+        myMap.addBuilding(new Building("Comstock House", "Mandelle Road"));
+        myMap.addBuilding(new Building("College Hall", "Seelye Dr"));
+        myMap.addBuilding(new Building("Wilson House","16 Kensington Ave"));
+        myMap.addBuilding(new Building("Emerson House", "Pardise Road"));
+        myMap.addBuilding(new Building("Morris House", "101 Green Street"));
         System.out.println(myMap);
     }
     
--- reflection.md
+++ reflection.md
@@ -1,4 +1,11 @@
 Use this file to record your reflection on this assignment.
 
 - Which methods did you decide to `overload`, and why?
+Cafe 
+- I overloaded makeCoffee to a default makeCoffee and a default makeCoffee if given a size because some people just want "a coffee" and some people just want a "small/large" coffee (meaning the boomers that get confused by flavor syrups).
+Library
+- I overloaded addTitle to also add an arraylist of titles if adding multiple titles. I also overloaded isAvailable to see all available titles instead of just checking if one title was available.
+House
+- I overloaded moveIn so that you can moveIn an arraylist of residents. I also did the same with moveOut.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I am confused about if this is working on A6 or A7 because how do I bring things from the subclasses to the CampusMap?
